f16324e9c6ab6eb218b72b9c4adc726deb727383
elaman sargaldaev <melisbekovelaman311@gmail.com>
2024-02-16 21:42:45 +0600

    написал логику касаемо user

--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import config from './config';
+import usersRouter from './routers/users';
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use(cors());
+app.use('/users', usersRouter);
+
 
 const run = async () => {
 
---
+++ api/models/User.ts
@@ -0,0 +1,64 @@
+import mongoose, {Model} from 'mongoose';
+import {UserFields} from '../types';
+import bcrypt from 'bcrypt';
+import {randomUUID} from 'crypto';
+
+
+interface UserMethods {
+  checkPassword(password: string): Promise<boolean>;
+  generateToken(): void;
+}
+
+type UserModel = Model<UserFields, {}, UserMethods>;
+
+const Schema = mongoose.Schema;
+
+const SALT_WORK_FACTOR = 10;
+
+const UserSchema = new Schema<UserFields, UserModel, UserMethods>({
+
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+
+  token: {
+    type: String,
+    required: true,
+  },
+});
+
+
+UserSchema.methods.generateToken = function () {
+  this.token = randomUUID();
+};
+
+UserSchema.methods.checkPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+
+  const hash = await bcrypt.hash(this.password, salt);
+
+  this.password = hash;
+  next();
+});
+
+UserSchema.set('toJSON', {
+  transform: (doc, ret, options) => {
+    delete ret.password;
+    return ret;
+  }
+});
+const User = mongoose.model<UserFields, UserModel>('User', UserSchema);
+export default User;
